cfd5586c11cd7cf07d74fba93a35b465c952446e
Roberto MF <git@virgulilla.com>
2018-06-21 11:41:52 +0200

    Rename common.defs.mk targets

--- src/apps/common.defs.mk
+++ src/apps/common.defs.mk
@@ -1,16 +1,22 @@
 export GPG_KEYSERVER := pgp.mit.edu
 
+export PACKAGE    := $(PACKAGE_NAME).apk
+export SOURCE_APK := $(PACKAGE_ID)_$(VERSION_CODE).apk
+export SOURCE_SIG := $(SOURCE_APK).asc
+
+
 .PHONY: all
 
-all: $(PACKAGE_NAME).apk
-$(PACKAGE_NAME).apk: $(PACKAGE_ID)_$(VERSION_CODE).apk
+all: $(PACKAGE)
+$(PACKAGE): $(SOURCE_APK)
 	@cp $< $@
 
-$(PACKAGE_ID)_$(VERSION_CODE).apk: $(PACKAGE_ID)_$(VERSION_CODE).apk.asc
-	@curl $(DOWNLOAD_URL) -o $@
+$(SOURCE_APK): $(SOURCE_SIG)
 	@gpg --quiet --list-key "$(REPO_GPG)" \
 	  || gpg --quiet --keyserver $(GPG_KEYSERVER) --recv-keys "$(REPO_GPG)"
-	@gpg --verify $< $@ || rm -f $@ $<
+	@curl $(DOWNLOAD_URL) -o $@
+	@gpg --verify $< $@ \
+	  || rm -f $@ $<
 
-$(PACKAGE_ID)_$(VERSION_CODE).apk.asc:
+$(SOURCE_SIG):
 	@curl $(DOWNLOAD_SIG) -o $@
